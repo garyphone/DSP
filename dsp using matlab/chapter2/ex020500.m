0e0c7a6b4838556d05cdc025ab7412cfab78e19c
garyphone <otto.gao24@gmail.com>
2018-02-26 17:09:31 +0100

    add new examples in chapter 2

---
+++ dsp using matlab/chapter2/ex020500.m
@@ -0,0 +1,44 @@
+% Example 2.5
+
+%
+
+% x(n)=[u(n)=u(n-10]; h(n)=(0.9)^n*u(n)
+
+% y(n)=10*(1-(0.9)^(n+1))*(u(n)-u(n-10))+
+
+%       (10*(1-(0.9)^10)*(0.9)^(n-9))*u(n-10)
+
+n = -5:50;
+
+u1 = stepseq(0,-5,50); u2=stepseq(10,-5,50);
+
+% input x(n)
+
+x = u1-u2;
+
+% impulse response h(n)
+
+h = ((0.9).^n).*u1;
+
+subplot(3,1,1); stem(n,x); axis([-5,50,0,2]);
+
+title('Input Sequence');
+
+xlabel('n'), ylabel('x(n)');
+
+subplot(3,1,2); stem(n,h); axis([-5,50,0,2]);
+
+title('Impulse Response');
+
+xlabel('n'), ylabel('h(n)');
+
+% output response
+
+y = (10*(1-(0.9).^(n+1))).*(u1-u2)+(10*(1-(0.9)^10)*(0.9).^(n-9)).*u2;
+
+subplot(3,1,3); stem(n,y); axis([-5,50,0,8]);
+
+title('Output Sequence');
+
+xlabel('n'), ylabel('y(n)');
+
